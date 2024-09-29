c0a1e865f48bb18eee63ccd70ea65e4373239b89
Jakob Sadjina <jakobsadjina@MacBook-Pro-von-Jakob.local>
2024-09-29 05:25:39 +0200

    ds

--- src/game/GameManager.cpp
+++ src/game/GameManager.cpp
@@ -2,10 +2,31 @@
 
 namespace game {
 
+#include <ApplicationServices/ApplicationServices.h>
+#include <iostream>
+
+void getScreenSizeMac() {
+    uint32_t displayCount;
+    CGGetActiveDisplayList(0, NULL, &displayCount);
+    std::cout << "Number of Active Displays: " << displayCount << std::endl;
+
+    CGDirectDisplayID displays[16];
+    CGGetActiveDisplayList(16, displays, &displayCount);
+
+    for (uint32_t i = 0; i < displayCount; ++i) {
+        CGRect displayBounds = CGDisplayBounds(displays[i]);
+        int width = displayBounds.size.width;
+        int height = displayBounds.size.height;
+        std::cout << "Display " << i + 1 << ": " << width << "x" << height << " pixels" << std::endl;
+    }
+}
+
+
 GameManager::GameManager():
 _window(sf::VideoMode::getDesktopMode(), "Chess", sf::Style::Default, sf::ContextSettings(0, 0, 16, 2, 0)) { //TODO dont hardcode size
     std::cout << _window.getSize().x << " " << _window.getSize().y << std::endl;
     _window.setFramerateLimit(60);
+    getScreenSizeMac();
 }
 
 void GameManager::run() {
